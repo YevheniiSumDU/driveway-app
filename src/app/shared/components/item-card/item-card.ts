e88968e1491fb6ead70387d772c0cdafb4e7ce58
Lobix <lobix2005@gmail.com>
2025-09-22 17:23:21 +0200

    pr-03-data-binding

--- src/app/shared/components/item-card/item-card.ts
+++ src/app/shared/components/item-card/item-card.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Car } from '../../models/car.model';
 
 @Component({
   selector: 'app-item-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './item-card.html',
   styleUrl: './item-card.scss',
 })
