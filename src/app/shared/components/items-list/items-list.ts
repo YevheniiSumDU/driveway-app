72397d32f64cac080d331aaf63943beaa6251f69
Lobix <lobix2005@gmail.com>
2025-10-14 18:58:35 +0200

    pr-10-http-client

--- src/app/shared/components/items-list/items-list.ts
+++ src/app/shared/components/items-list/items-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -13,15 +13,43 @@ import { Observable } from 'rxjs';
   templateUrl: './items-list.html',
   styleUrl: './items-list.scss',
 })
-export class ItemsList {
+export class ItemsList implements OnInit {
   searchQuery = '';
   cars$: Observable<Car[]>;
+  isLoading = true;
+  error: string | null = null;
 
   constructor(private dataService: DataService) {
     this.cars$ = this.dataService.getItemsStream();
   }
 
+  ngOnInit() {
+    this.dataService.getItems().subscribe({
+      next: () => {
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.error = error.message;
+      },
+    });
+  }
+
   onSearchChange() {
     this.dataService.filterItems(this.searchQuery);
+  }
+
+  retryLoading() {
+    this.isLoading = true;
+    this.error = null;
+    this.dataService.getItems().subscribe({
+      next: () => {
+        this.isLoading = false;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.error = error.message;
+      },
+    });
   }
 }
